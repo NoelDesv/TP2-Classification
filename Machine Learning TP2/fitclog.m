a7308c1ca5bd0f5bcb635a80e34a8f73d3aa1f43
NoelDesv <32549601+NoelDesv@users.noreply.github.com>
2018-10-16 18:24:39 +0400

    Update fitclog.m

--- Machine Learning TP2/fitclog.m
+++ Machine Learning TP2/fitclog.m
@@ -1,4 +1,4 @@
-function [ W, ersub ] = fitclog(X, T, optimStruct)
+function [ W, ersub ] = fitclog(X, T, optimStruct, options)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     %XTild = ones(setSize,nb_entree(2)+1,degree);
@@ -9,23 +9,25 @@ function [ W, ersub ] = fitclog(X, T, optimStruct)
     for ii=2:setSize(2)+1
         phi(:,ii) = phi(:,ii).*X(:,ii-1);
     end
-    w = ones(nbClass, degree*setSize(2)+1);
+    W = ones(degree*setSize(2)+1, nbClass);
     size(phi)
-    disp(w)
+    disp(W)
     %DESCENTE DE GRADIENT
     ii = 1;
     sensitivity = 1;
     while (sensitivity>optimStruct.tolerance && ii < optimStruct.nbIteration)
-        wprev = w;
-        disp(sigmoid(phi*w));
-        %grad = (phi')*(sigmoid(phi*w)-T); 
+        wprev = W;
+        %disp(phi);
+        %disp(W);
+        %disp(softmax(phi*W));
+        grad = (phi')*(softmax(phi*W)-T); 
         %grad = (phi')*(softmax(phi*w)-t);
-        w = w - optimStruct.gamma*grad;
-        sensitivity = norm(w - wprev)/norm(w);
+        W = W - optimStruct.gamma*grad;
+        sensitivity = norm(W - wprev)/norm(W);
         ii = ii + 1;
-        lossfunct(ii) = 0.5 * norm(phi*w-t);
+        ersub(ii) = 0.5 * norm(phi*W-T);
         if options.disp == 2 %VISUALISATION DE LA FONCTION D'ERREUR 
-            plot(1:ii,lossfunct(1:ii))
+            plot(1:ii,ersub(1:ii))
         end
     end
 
