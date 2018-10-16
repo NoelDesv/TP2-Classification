18e240e44704c86a5d33243c3120ec8d66f9eca5
NoelDesv <32549601+NoelDesv@users.noreply.github.com>
2018-10-16 17:45:54 +0400

    Modification reg_log

--- Machine Learning TP2/fitclog.m
+++ Machine Learning TP2/fitclog.m
@@ -12,7 +12,22 @@ function [ W, ersub ] = fitclog(X, T, optimStruct)
     w = ones(nbClass, degree*setSize(2)+1);
     size(phi)
     disp(w)
-    grad = (phi')*(phi*w-T);
+    %DESCENTE DE GRADIENT
+    ii = 1;
+    sensitivity = 1;
+    while (sensitivity>optimStruct.tolerance && ii < optimStruct.nbIteration)
+        wprev = w;
+        disp(sigmoid(phi*w));
+        %grad = (phi')*(sigmoid(phi*w)-T); 
+        %grad = (phi')*(softmax(phi*w)-t);
+        w = w - optimStruct.gamma*grad;
+        sensitivity = norm(w - wprev)/norm(w);
+        ii = ii + 1;
+        lossfunct(ii) = 0.5 * norm(phi*w-t);
+        if options.disp == 2 %VISUALISATION DE LA FONCTION D'ERREUR 
+            plot(1:ii,lossfunct(1:ii))
+        end
+    end
 
 end
 
